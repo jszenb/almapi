fa7c57d971ff68d5a8a64b659b117ab626d18809
jszenb <jszenb@gmail.com>
2024-03-29 14:18:48 +0100

    First commit

--- lib/almapi.rb
+++ lib/almapi.rb
@@ -3,6 +3,15 @@
 require_relative "almapi/version"
 
 module Almapi
+  class Bonjour
+    def self.say_hi
+      "Hello world!"
+    end
+
+    def self.say_bye
+      "Bye world!"
+    end
+  end
   class Error < StandardError; end
   # Your code goes here...
 end
--- spec/almapi_spec.rb
+++ spec/almapi_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe Almapi do
   it "has a version number" do
     expect(Almapi::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Almapi::Bonjour do
+  it "says hi" do
+    expect(Almapi::Bonjour.say_hi).to eq("Hello world!")
+  end
+
+  it "says bye" do
+    expect(Almapi::Bonjour.say_bye).to eq("Bye world!")
   end
 end
