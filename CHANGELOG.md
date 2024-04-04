f62a8a160ba0158f41ae9d36ae59ac3a3e390bc5
jszenb <jerome.chiavassa-szenberg@campus-condorcet.fr>
2024-04-04 11:23:08 +0200

    Version 0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 ## [Unreleased]
 
+## [0.1.2] - 2024-04-04
+
+- Correct require_relative error (learning to create gem so may take a few versions :) .
+
+## [0.1.1] - 2024-04-04
+
+- Correct require_relative error.
+
 ## [0.1.0] - 2024-03-29
 
 - Initial release
--- lib/almapi.rb
+++ lib/almapi.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "almapi/version"
-require_relative 'almapi/almapi_error'
-require_relative 'almapi/almapi'
+require_relative "./almapi/version"
+require_relative './almapi/almapi_error'
+require_relative './almapi/almapi'
 
 # Module Almapi handles Alma's API call and errors
 # @author jszenb
